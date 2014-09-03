c7cbff9345c44550ed4ca81f6214530e303a2857
基酷 <jiku.wxy@alibaba-inc.com>
2014-09-03 11:01:45 +0800

    tag 0.0.3 and add bin

---
+++ bin/dir-generate.js
@@ -0,0 +1,2 @@
+#!/usr/bin/env node
+require('../lib/dir-generate.js').run();
--- index.js
+++ index.js
@@ -1,1 +1,1 @@
-module.exports = require('./lib/dir-generate.js');
+module.exports = require('./lib/dir-generate.js').run();
